bc42414dc4320e54da11ca3b5fb71c9464f2a206
Nicholas Chin <nic.c3.14@gmail.com>
2021-08-25 22:31:00 -0600

    Tweaked makefile for coreboot, add license headers
    Changed makefile to better integrate with coreboot build system, and
    added license headers to fileio

--- doomgeneric/fileio.c
+++ doomgeneric/fileio.c
@@ -1,3 +1,20 @@
+/*
+ * Copyright(C) 2021 Nicholas Chin
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * Emulates the IO functions in stdio.h using memory buffers,
+ * loading files off CBFS
+ */
+
 #include "fileio.h"
 #include <cbfs.h>
 
